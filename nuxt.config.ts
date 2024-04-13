11f75a7cfb1ab3c0a2eb8f69cf50068e13bf3e69
jeongsh <pandapupu88@naver.com>
2024-04-13 15:29:56 +0900

    nitro기본제공 websocket로 변경해봄

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   app:{
     baseURL: '/websocket/'
+  },
+  modules: ['@vueuse/nuxt'],
+  nitro: {
+    experimental: {
+      websocket: true
+    }
   }
 })
