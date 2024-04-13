e2b23da286ef90db0ec03d43e282f7a06520f6d5
jeongsh <pandapupu88@naver.com>
2024-04-13 15:51:48 +0900

    유저 아이디 기능 추가

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-websocket을 이해하려고 만들어본 가장 간단한 기능
-nitro에서 제공하는 websocket이용
+# websocket을 이해하려고 만들어본 가장 간단한 기능
 
-참고영상 : https://www.youtube.com/watch?v=OfY7JcrqkPg
-코드 : https://github.com/manniL/alexander-lichter-websockets-nitro/
-nitro websocket 공식 문서 : https://nitro.unjs.io/guide/websocket
+## nitro에서 제공하는 websocket이용
 
-vue use 온갖 잡것을 쉽게 사용하게 해주는 라이브러리 - 개좋은거 같음
+참고영상 : https://www.youtube.com/watch?v=OfY7JcrqkPg   
+nitro websocket 공식 문서 : https://nitro.unjs.io/guide/websocket   
+(공식문서는 route 폴더안에 뒀는데 이 예시는 api폴더에둠 근데 api가 맞는거 같음)
+
+## 사용된 라이브러리
+vue use    
+온갖 잡것을 쉽게 사용하게 해주는 라이브러리 - 개좋은거 같음   
 https://vueuse.org/
+
+uuid   
+클라이언트에 고유한 id를 부여하기 위해 사용
--- components/WebSocket.client.vue
+++ components/WebSocket.client.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <h1>WebSocket - let's go!</h1>
+    <input v-model="myName" placeholder="내 이름 입력">
     <form @submit.prevent="sendData">
-      <input v-model="message">
+      <input v-model="message" placeholder="메세징 입력">
       <button type="submit">Send</button>
     </form>
     <div>
@@ -12,18 +12,27 @@
 </template>
 
 <script setup lang="ts">
-// In prod: check if secure, then use wss://
-const { status, data, send, open, close } = useWebSocket(`ws://${location.host}/api/websocket`)
+import { v4 as uuidv4 } from 'uuid'; // UUID 생성을 위한 라이브러리
 
-const history = ref<string[]>([])
+const userId = uuidv4(); // 고유한 유저 ID 생성
+const myName = ref(''); // 내 이름
+// WebSocket 설정
+const { status, data, send, open, close } = useWebSocket(`ws://${location.host}/api/websocket`, {
+  onOpen: () => {
+    // 연결이 열리면 서버에 userId를 전송
+    send(JSON.stringify({ type: 'userId', data: userId }));
+  }
+});
+
+const history = ref<string[]>([]);
 watch(data, (newValue : any) => {
-  history.value.push(`server: ${newValue}`)
-})
+  history.value.push(`server: ${newValue}`);
+});
 
-const message = ref('')
+const message = ref('');
 function sendData(){
-  history.value.push(`client: ${message.value}`)
-  send(message.value)
-  message.value = ''
+  history.value.push(`client: ${message.value}`);
+  send(message.value);
+  message.value = '';
 }
 </script>
